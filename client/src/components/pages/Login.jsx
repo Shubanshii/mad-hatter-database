5a5967b4e51b5b39ae27c879e05f521b8e6e8eda
Chris Day <c_day@alumni.baylor.edu>
2019-05-23 13:59:34 -0500

    input vals in login

--- client/src/components/pages/Login.jsx
+++ client/src/components/pages/Login.jsx
@@ -32,8 +32,8 @@ export default class Login extends Component {
       <div className="Login">
         <h2>Login</h2>
         <form>
-          Username: <input type="text" value={this.state.username} name="username" onChange={this.handleInputChange} /> <br />
-          Password: <input type="password" value={this.state.password} name="password" onChange={this.handleInputChange} /> <br />
+          Username: <input type="text" value='TestUser' name="username" onChange={this.handleInputChange} /> <br />
+          Password: <input type="password" value='testpass' name="password" onChange={this.handleInputChange} /> <br />
           <button onClick={(e) => this.handleClick(e)}>Login</button>
         </form>
         {this.state.message && <div className="info info-danger">
